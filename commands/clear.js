a1068123bfd301c72a8f8d146fad89a1f1209cee
hyakimaro <45545324+byhyakimaro@users.noreply.github.com>
2021-05-01 14:41:49 -0300

    Delete clear.js

--- commands/clear.js
+++
@@ -1,8 +0,0 @@
-module.exports = async (client,msg) =>{
-    try{
-        const channel  = msg.channel;
-        const FetchMsg  = await channel.messages.fetch();
-        await channel.bulkDelete(FetchMsg, true);
-    } catch(error) {
-        msg.channel.send(error)}
-}
