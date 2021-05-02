baf8bb21ff715eeb21618f14bcef6c996d49510a
hyakimaro <45545324+byhyakimaro@users.noreply.github.com>
2021-05-01 22:09:07 -0300

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -1,6 +1,5 @@
 const Discord   = require("discord.js");
 const config    = require("../config.json");
-const commands  = require("../scripts/commandsReader")(config.prefix);
 
 module.exports = async (client,msg) =>{msg.delete();
     let emoji = await msg.guild.emojis.cache.find(emoji => emoji.name === "fix");
