ac9eada425241d66102a124c830c1db71a244209
hyakimaro <45545324+byhyakimaro@users.noreply.github.com>
2021-05-02 12:31:47 -0300

    Delete database directory

--- database/db.js
+++
@@ -1,6 +0,0 @@
-const mysql = require("mysql");
-const config = require("../config.json");
-
-module.exports = {
-  'mysql': mysql.createConnection({host: (config.host),user: (config.user),password: (config.password),database: (config.database)})
-}
