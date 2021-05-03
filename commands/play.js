be01ee7df9182cee5a0088340c529710c5b16ce5
hyakimaro <45545324+byhyakimaro@users.noreply.github.com>
2021-05-03 05:08:51 -0300

    Add files via upload

--- commands/play.js
+++ commands/play.js
@@ -1,27 +1,31 @@
-const Discord   = require("discord.js");
-const db = require('../scripts/db');
-const config = require("../config.json");
 const ytSearch = require( 'yt-search');
 const ytdl = require('ytdl-core');
 
-module.exports = async (client, msg, args) => {
-    const songName = args.join(' ');
-    if(!songName) return msg.reply('seu sapinho :D');
-    async function searchSong(title) {
-        const { videos } = await ytSearch(title);
-        const found = videos[0];
-        if(!found) return;
-        return ytdl(found.url);
-    };
-    const voiceChannel = msg.member.voice.channel;
-    if(!voiceChannel) return msg.reply('seu sapo');
-    searchSong(songName).then(async(song) => {
-        if(!song) return msg.reply('seu sapo');
-        const connection = await voiceChannel.join();
-        const dispatcher = connection.play(song);
-        dispatcher.on('finish', () => {
-            msg.reply('cabou');
-            voiceChannel.leave();
+module.exports = {
+    name: "play",
+    aliases: ["p"],
+    run: async (client, msg, args) => {
+        const songName = args.join(' ');
+        const queue = [];
+        if(!songName) return msg.reply('Escreva');
+        async function searchSong(title) {
+            const { videos } = await ytSearch(title);
+            const found = videos[0];
+            if(!found) return;
+            return ytdl(found.url);
+        };
+        const voiceChannel = msg.member.voice.channel;
+        if(!voiceChannel) return msg.reply('seu sapo');
+        searchSong(songName).then(async(song) => {
+            queue.push(song);
+            if(!song) return msg.reply('seu sapo');
+            const connection = await voiceChannel.join();
+            const dispatcher = connection.play(song);
+            dispatcher.on('finish', () => {
+                queue.splice(song);
+                msg.reply('cabou');
+                voiceChannel.leave();
+            });
         });
-    });
+    }
 };
