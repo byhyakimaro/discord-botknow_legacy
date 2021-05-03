d1bba5dab12cf580cfd6f8389efa21cb547bfdcd
hyakimaro <45545324+byhyakimaro@users.noreply.github.com>
2021-05-03 05:08:31 -0300

    Add files via upload

--- scripts/commandsReader.js
+++ scripts/commandsReader.js
@@ -2,12 +2,11 @@ const fs = require("fs");
 const dir = "./commands/";
 
 module.exports = (prefix) =>{
-    var commands = {};
+    var commands = [];
 
     const scripts = fs.readdirSync(dir);
     scripts.forEach(script=>{
-        commands[prefix+script.split(".")[0]] = require("../"+dir+script);
+        commands.push(require("../"+dir+script));
     });
-
     return commands;
 }
