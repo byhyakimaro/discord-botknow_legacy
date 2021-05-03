0812029a7c89e8eb68066f237205165de8e34404
hyakimaro <45545324+byhyakimaro@users.noreply.github.com>
2021-05-03 05:46:39 -0300

    Add files via upload

--- commands/clear.js
+++ commands/clear.js
@@ -1,11 +1,16 @@
-module.exports = async (client,msg) =>{
-    if(!msg.member.permissions.has("MANAGE_MESSAGES")) {
-    	return msg.channel.send("**Acess Denied. :lock:**").then(msg => msg.delete({timeout: 5000}))
-  	}
-    try{
-        const channel  = msg.channel;
-        const FetchMsg  = await channel.messages.fetch();
-        await channel.bulkDelete(FetchMsg, true);
-    } catch(error) {
-        msg.channel.send(error)}
-}
+module.exports = {
+    name: "clear",
+    aliases: [],
+    run: async (client, msg, args) => {msg.delete();
+        if(!msg.member.permissions.has("MANAGE_MESSAGES")) {
+            return msg.channel.send("**Acess Denied. :lock:**").then(msg => msg.delete({timeout: 5000}))
+        }
+        try{
+            const channel  = msg.channel;
+            const FetchMsg  = await channel.messages.fetch();
+            await channel.bulkDelete(FetchMsg, true);
+        } catch(error) {
+            msg.channel.send(error)
+        };
+    }
+};
