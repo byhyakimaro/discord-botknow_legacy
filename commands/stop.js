e77a2d5ae41f91d7e5a783c2737b7ba41b1599c1
hyakimaro <45545324+byhyakimaro@users.noreply.github.com>
2021-05-03 17:26:26 -0300

    Add files via upload

--- commands/stop.js
+++ commands/stop.js
@@ -2,9 +2,13 @@ const Discord   = require("discord.js");
 const db = require('../scripts/db');
 const config = require("../config.json");
 
-module.exports = async (client, msg) => {
-    const voiceChannel = msg.member.voice.channel;
-	if (!voiceChannel) return msg.reply(`You must be in a voice channel to use this command.`).then((m)=>{ m.delete({timeout: 4000}).catch(() => null) });
-    voiceChannel.leave();
-    msg.reply('Song Stopped!');
+module.exports = {
+    name: "stop",
+    aliases: ["s"],
+    run: async (client, msg, args) => {
+        const voiceChannel = msg.member.voice.channel;
+        if (!voiceChannel) return msg.reply(`You must be in a voice channel to use this command.`).then((m)=>{ m.delete({timeout: 4000}).catch(() => null) });
+        voiceChannel.leave();
+        msg.react('🛑');
+    }
 };
