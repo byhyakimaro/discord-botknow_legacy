1e1a431a6a138dacb0798ec9ff4a80b9540b932c
hyakimaro <45545324+byhyakimaro@users.noreply.github.com>
2021-05-04 16:56:47 -0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+client.queues = new Map();
 
 const config = require("./config.json");
 const db = require('./scripts/db');
