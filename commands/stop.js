aad96285daf611d2da7deb95664f3c8acbf7b49a
hyakimaro <45545324+byhyakimaro@users.noreply.github.com>
2021-05-04 20:20:20 -0300

    Add files via upload

--- commands/stop.js
+++ commands/stop.js
@@ -4,13 +4,13 @@ const config = require("../config.json");
 
 module.exports = {
     name: "stop",
-    aliases: ["s"],
+    aliases: [],
     help: "para a musica do bot.",
     run: async (client, msg, args) => {
         const voiceChannel = msg.member.voice.channel;
         if (!voiceChannel) return msg.reply(`You must be in a voice channel to use this command.`).then((m)=>{ m.delete({timeout: 4000}).catch(() => null) });
         voiceChannel.leave();
-        client.queues.delete(msg.member.guild.id);
+        client.queues.delete(msg.member.guild.id)
         msg.react('🛑');
     }
 };
