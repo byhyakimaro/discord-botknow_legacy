5c958f52237dd9108e43fefbb8d1ae969e5af6e0
hyakimaro <45545324+byhyakimaro@users.noreply.github.com>
2021-05-04 18:44:30 -0300

    Add files via upload

---
+++ commands/volume.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const db = require('../scripts/db');
+const config = require("../config.json");
+
+module.exports = {
+    name: "volume",
+    aliases: ["vol"],
+    run: async (client, msg) => {
+        const args = msg.content.split(" ");
+        const volume = Number(args[1]);
+        const queue = client.queues.get(msg.guild.id);
+        const voiceChannel = msg.member.voice.channel;
+        if (!voiceChannel) return msg.reply(`You must be in a voice channel to use this command.`).then((m)=>{ m.delete({timeout: 5000}).catch(() => null) });
+        if (isNaN(volume) || volume < 0 || volume > 200) return msg.reply(`Insira um valor entre 0 e 200`);
+        if (!queue) return msg.reply(`no songs in currently playing in this guild.`);
+        queue.dispatcher.setVolume(volume/100);
+        queue.volume = volume;
+        client.queues.set(msg.guild.id, queue);
+        msg.react('✅');
+    }
+};
