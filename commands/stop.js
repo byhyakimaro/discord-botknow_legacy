db30bcd4b0a700b424fa2c72221014b18503b88e
hyakimaro <45545324+byhyakimaro@users.noreply.github.com>
2021-05-04 20:24:27 -0300

    Add files via upload

--- commands/stop.js
+++ commands/stop.js
@@ -8,9 +8,11 @@ module.exports = {
     help: "para a musica do bot.",
     run: async (client, msg, args) => {
         const voiceChannel = msg.member.voice.channel;
+        const queue = client.queues.get(msg.guild.id);
         if (!voiceChannel) return msg.reply(`You must be in a voice channel to use this command.`).then((m)=>{ m.delete({timeout: 4000}).catch(() => null) });
+        if (!queue) return msg.reply(`no songs in currently playing in this guild.`);
         voiceChannel.leave();
-        client.queues.delete(msg.member.guild.id)
+        client.queues.delete(msg.member.guild.id);
         msg.react('🛑');
     }
 };
