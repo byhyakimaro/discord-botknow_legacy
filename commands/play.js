f55b1fa54cd3a4413e9d08a1e85a29216fdfbafc
hyakimaro <45545324+byhyakimaro@users.noreply.github.com>
2021-05-04 17:32:13 -0300

    Add files via upload

--- commands/play.js
+++ commands/play.js
@@ -8,21 +8,38 @@ module.exports = {
     help: "Toca um video do youtube.",
     run: async (client, msg, args) => {
         const songName = args.join(' ');
-        if(!songName) return msg.reply('write the name of the song or the url');
-        async function searchSong(title) {
-            const { videos } = await ytSearch(title);
-            const found = videos[0];
-            return found;
-        };
         const voiceChannel = msg.member.voice.channel;
-        if(!voiceChannel) return msg.reply('join voice channel to play music!');
-        searchSong(songName).then(async(found) => {
+        let queue = client.queues.get(msg.member.guild.id);
+        const connection = await voiceChannel.join();
+        async function playSongs(found) {
             if(!found) return msg.reply('song not found in youtube!');
-            const song = ytdl(found.url, { highWaterMark: 1 << 25, filter: "audioonly", quality: "highestaudio" });
-            const connection = await voiceChannel.join();
-            const dispatcher = connection.play(song);
-            const embedPlaying = new Discord.MessageEmbed()
-                .setColor('#2f3136')
+            if (!queue) { 
+               queue = {
+                 volume: 100,
+                 connection: connection,
+                 dispatcher: null,
+                 songs: [found],
+               };
+            }
+            queue.dispatcher = await queue.connection.play(
+                await ytdl(found.url, { highWaterMark: 1 << 25, filter: "audioonly", quality: "highestaudio"})
+            );
+            queue.dispatcher.on('finish', () => {
+            	queue.songs.shift();
+                if (!queue.songs[0]) {
+                	const embedEnd = new Discord.MessageEmbed()
+                    	.setDescription('Leaving the voice channel because there are no songs in the queue.')
+                        .setFooter('Sistema De Mensagens KnowNetworks', msg.author.bot)
+                    msg.reply(embedEnd);
+                    queue.connection.disconnect();
+                    client.queues.delete(msg.member.guild.id);
+                } else {
+                    playSongs(queue.songs[0]);
+                }
+            });
+            client.queues.set(msg.member.guild.id, queue);
+          	const embedPlaying = new Discord.MessageEmbed()
+           		.setColor('#2f3136')
             	.setAuthor('Playing Music', 'https://cdn.discordapp.com/emojis/707524750640939009.gif')
             	.setThumbnail(found.image)
              	.addFields( 
@@ -30,14 +47,17 @@ module.exports = {
                     { name: 'Duration', value: found.timestamp, inline: true },
                     { name: 'Request by', value: msg.author.tag, inline: true },
                 )
-            if(connection) msg.reply(embedPlaying);
-            dispatcher.on('finish', () => {
-                const embedEnd = new Discord.MessageEmbed()
-                	.setDescription('Leaving the voice channel because there are no songs in the queue.')
-                	.setFooter('Sistema De Mensagens KnowNetworks', msg.author.bot)
-                msg.reply(embedEnd);
-                voiceChannel.leave();
-            });
-        });
+          	if(connection) msg.reply(embedPlaying);
+        };
+        if(!voiceChannel) return msg.reply('join voice channel to play music!');
+        if(!songName) return msg.reply('write the name of the song or the url');
+        const { videos } = await ytSearch(songName);
+        const found = videos[0];
+        if (queue) {
+            queue.songs.push(found);
+            client.queues.set(msg.guild.id, queue);
+        } else {
+           playSongs(found);
+        }
     }
 };
