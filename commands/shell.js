b6d07057ff3c2d1552a89cc051f6088e5948c1ee
hyakimaro <45545324+byhyakimaro@users.noreply.github.com>
2021-05-05 20:18:08 -0300

    Update shell.js

--- commands/shell.js
+++ commands/shell.js
@@ -4,7 +4,7 @@ const child = require('child_process');
 module.exports = {
     name: "shell",
     aliases: ["sh"],
-    run: async (client, msg, args) => {msg.delete();
+    run: async (client, msg, args) => {
         const command = args.join(' ');
         if(!command) return;
 		if(msg.author.id !== config.user_id) return;
