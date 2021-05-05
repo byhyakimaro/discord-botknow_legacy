36cb88cff134b66ab1211c43a19476d8d46e8b3e
hyakimaro <45545324+byhyakimaro@users.noreply.github.com>
2021-05-05 04:23:35 -0300

    Add files via upload

--- commands/remove.js
+++ commands/remove.js
@@ -8,11 +8,12 @@ module.exports = {
     help: "remove a musica da playlist.",
     run: async (client, msg) => {
         const song = Number(msg.content.split(" ")[1]);
-        if (!song) return msg.reply('escreva o numero da musica na playlist.');
+        if (!song) return msg.reply('selecione o numero da musica na playlist.');
         const voiceChannel = msg.member.voice.channel;
         const queue = client.queues.get(msg.guild.id);
         if (!voiceChannel) return msg.reply(`You must be in a voice channel to use this command.`).then((m)=>{ m.delete({timeout: 4000}).catch(() => null) });
         if (!queue) return msg.reply(`no songs in currently playing in this guild.`);
+        if (queue.songs.length > song || queue.songs.length < song ) return msg.reply('selecione uma musica da playlist.');
         queue.songs.splice(song-1);
         msg.react('✅');
     }
