ebd7ade5297dcb5672ab2b1fddd4ed8969dbbe44
hyakimaro <45545324+byhyakimaro@users.noreply.github.com>
2021-05-05 03:16:32 -0300

    Add files via upload

---
+++ commands/queue.js
@@ -0,0 +1,25 @@
+const Discord   = require("discord.js");
+const db = require('../scripts/db');
+const config = require("../config.json");
+
+module.exports = {
+    name: "queue",
+    aliases: ["qu"],
+    help: "lista todas as musicas da playlist.",
+    run: async (client, msg, args) => {
+        let songs = '';
+        const voiceChannel = msg.member.voice.channel;
+        const queue = client.queues.get(msg.guild.id);
+        if (!voiceChannel) return msg.reply(`You must be in a voice channel to use this command.`).then((m)=>{ m.delete({timeout: 4000}).catch(() => null) });
+        for (let i=0; i < queue.songs.length; i++) {
+          const song = queue.songs[i];
+          songs += `\n**${i+1}) ${song.title}** \`(${song.timestamp})\``;
+        };
+        const embed = new Discord.MessageEmbed()
+        	.setColor('#faed70')
+        	.setTitle('Musics queue.')
+        	.setDescription(`\n${songs}`)
+        	.setFooter('By KnowNetworks')
+        msg.reply(embed);
+    }
+};
