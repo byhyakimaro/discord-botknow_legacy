51529d17bbc0aab0e3e80facb1e07de98c8ee24f
hyakimaro <45545324+byhyakimaro@users.noreply.github.com>
2021-05-04 23:03:51 -0300

    Add files via upload

---
+++ commands/resume.js
@@ -0,0 +1,17 @@
+const Discord   = require("discord.js");
+const db = require('../scripts/db');
+const config = require("../config.json");
+
+module.exports = {
+    name: "resume",
+    aliases: [],
+    help: "retoma a ultima musica do bot.",
+    run: async (client, msg, args) => {
+        const voiceChannel = msg.member.voice.channel;
+        const queue = client.queues.get(msg.guild.id);
+        if (!voiceChannel) return msg.reply(`You must be in a voice channel to use this command.`).then((m)=>{ m.delete({timeout: 4000}).catch(() => null) });
+        if (!queue) return msg.reply(`no songs in currently playing in this guild.`);
+  		queue.dispatcher.resume();
+        msg.react('▶');
+    }
+};
