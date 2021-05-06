95f81282e31fd19aedbed242c7d37585907c8295
hyakimaro <45545324+byhyakimaro@users.noreply.github.com>
2021-05-06 18:38:11 -0300

    Add files via upload

--- commands/eval.js
+++ commands/eval.js
@@ -5,7 +5,10 @@ module.exports = {
     name: "eval",
     aliases: [],
     run: async (client, msg, args) => {
-        const command = args.join(' ');
+        const command = args.join(' ')
+            .replace(/^`{3}(js)?|`{3}$/g, '')
+            .replace(/<@!?(\d{16,18})>/g, '_user($1)');
+        const _user = (id) => client.users.cache.find((user) => user.id == id);
 		if(msg.author.id !== config.user_id) return;
         const evaled = async (command) => { return eval(command) };
        	evaled(command)
