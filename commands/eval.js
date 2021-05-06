6a36fe3c1901deaba186acf6f1f5aa824edfb88a
hyakimaro <45545324+byhyakimaro@users.noreply.github.com>
2021-05-05 21:32:20 -0300

    Add files via upload

--- commands/eval.js
+++ commands/eval.js
@@ -1,15 +1,15 @@
 const config = require("../config.json");
+const { inspect } = require('util');
 
 module.exports = {
     name: "eval",
     aliases: [],
     run: async (client, msg, args) => {
         const command = args.join(' ');
-        if(!command) return;
 		if(msg.author.id !== config.user_id) return;
         const evaled = async (command) => { return eval(command) };
        	evaled(command)
-            .then((res)=> msg.channel.send(res, {code: 'js'}))
-            .catch((err)=> msg.channel.send(err, {code: 'js'}));
+        	.then((res)=> msg.channel.send(inspect(res), {code: 'js'}))
+        	.catch((err)=> msg.channel.send(inspect(err), {code: 'js'}));
     }
 };
