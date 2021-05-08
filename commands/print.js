6f0f692fb157c743e41d2d6a3648b1fba051e96b
hyakimaro <45545324+byhyakimaro@users.noreply.github.com>
2021-05-07 21:50:48 -0300

    Add files via upload

--- commands/print.js
+++ commands/print.js
@@ -4,49 +4,43 @@ const config = require("../config.json");
 
 let browser, page;
 
-async function webScrapping(url) {
-    const ppet = puppeteer.launch({
-        headless: true,
-        args: [
-            "--disable-gpu",
-            "--disable-dev-shm-usage",
-            "--no-sandbox",
-            "--disable-setuid-sandbox"
-        ]
-    }).then(async (chrome) => {
-        browser = chrome;
-        page = await browser.newPage();
-      
-        await page.setViewport({
-            width: 1366,
-            height: 768
-        });
-
-        await page.goto(url);
-        const screenshoot = await page.screenshot();
-
-        await browser.close();
-        return screenshoot;
+puppeteer.launch({
+    headless: true,
+    args: [
+        "--disable-gpu",
+        "--disable-dev-shm-usage",
+        "--no-sandbox",
+        "--disable-setuid-sandbox"
+    ]
+}).then(async (chrome) => {
+    browser = chrome;
+    page = await browser.newPage();
+  
+    await page.setViewport({
+        width: 1366,
+        height: 768
     });
-    return ppet;
-};
+});
 
 module.exports = {
     name: "print",
     aliases: [],
     run: async (client, msg, args) => {
- 		let url = args.join(' ');
-      
+        let url = args.join(' ');    
         if(!url) return msg.reply('envie um url');
-        if(!url.startsWith('http')) url = 'https://' + url;
-      
+        if(!url.startsWith('http')) url = 'https://' + url;   
         const loading = await msg.reply('carregando...');
-        console.log(await webScrapping(url));
-        /*.then((m)=> {
-            const attachment = new MessageAttachment(m);
+      
+        await page.goto(url)
+        .then(async()=>{
+            const screenshoot = await page.screenshot();
+            const attachment = new MessageAttachment(screenshoot);
             msg.channel.send(attachment);
         })
-        .catch(()=> { return msg.reply('Pagina não encontrada.') });
-        loading.delete();*/
+        .catch((err)=>{
+            console.log(err);
+            msg.channel.send('Pagina Não Encontrada.');
+        })
+        loading.delete();
     }
 };
