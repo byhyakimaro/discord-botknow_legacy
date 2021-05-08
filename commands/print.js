ce0f04b03b4da187fb9f16b0c4ce328bfce45bcd
hyakimaro <45545324+byhyakimaro@users.noreply.github.com>
2021-05-07 22:32:09 -0300

    Add files via upload

--- commands/print.js
+++ commands/print.js
@@ -2,9 +2,8 @@ const puppeteer = require('puppeteer');
 const { MessageAttachment } = require('discord.js');
 const config = require("../config.json");
 
-let browser, page;
-
-puppeteer.launch({
+const putt = async (url) => {
+   const putt = puppeteer.launch({
     headless: true,
     args: [
         "--disable-gpu",
@@ -12,26 +11,29 @@ puppeteer.launch({
         "--no-sandbox",
         "--disable-setuid-sandbox"
     ]
-}).then(async (chrome) => {
-    browser = chrome;
-    page = await browser.newPage();
-  
-    await page.setViewport({
-        width: 1366,
-        height: 768
+    }).then(async (chrome) => {
+        let browser = chrome;
+        let page = await browser.newPage();
+
+        await page.setViewport({
+            width: 1366,
+            height: 768
+        });
+        const res = await page.goto(url)
+        return res;
     });
-});
+    return putt;
+}
 
 module.exports = {
     name: "print",
     aliases: [],
     run: async (client, msg, args) => {
-        let url = args.join(' ');    
+        let url = args.join(' ');
         if(!url) return msg.reply('envie um url');
         if(!url.startsWith('http')) url = 'https://' + url;   
         const loading = await msg.reply('carregando...');
-      
-        await page.goto(url)
+        putt(url)
         .then(async()=>{
             const screenshoot = await page.screenshot();
             const attachment = new MessageAttachment(screenshoot);
@@ -39,7 +41,7 @@ module.exports = {
         })
         .catch((err)=>{
             console.log(err);
-            msg.channel.send('Pagina Não Encontrada.');
+            msg.reply('Pagina Não Encontrada.');
         })
         loading.delete();
     }
