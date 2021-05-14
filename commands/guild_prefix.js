a970dcf7f3e42c6c47ba8f4559171845f206e7dc
hyakimaro <45545324+byhyakimaro@users.noreply.github.com>
2021-05-14 15:32:16 -0300

    Add files via upload

--- commands/guild_prefix.js
+++ commands/guild_prefix.js
@@ -10,20 +10,18 @@ module.exports = {
         if (msg.member.id !== config.user_id) return msg.channel.send("**Acess Denied. :lock:**").then(msg => msg.delete({timeout: 4000}));
         const args = msg.content.split(" ");
 		if (!args[1]) return msg.reply('Escreva o id do servidor');
-        db.mysql.query("SELECT guild_id,prefix FROM prefixs WHERE guild_id= ?", [Number(args[1])], async function (err, result) {
-            if (err) return console.log(err);
-            if (result == "") return msg.reply('Servidor não encontrado.')
-                .then((m) => { m.delete({timeout: 4000}).catch(() => null)} );
-            const prefix = JSON.parse(JSON.stringify(result[0].prefix));
-            let emoteFix = await client.emojis.cache.get("755967379363856455");
-            let embed = new Discord.MessageEmbed()
-              .setColor("#000000")
-              .setTitle(`${emoteFix} **Prefixo ${client.guilds.cache.get(args[1]).name}!**`)
-              .setDescription(`\`\`\`o prefixo de ${client.guilds.cache.get(args[1]).name} é ${prefix}\`\`\``)
-              .setThumbnail(client.guilds.cache.get(args[1]).iconURL())
-              .setFooter("Sistema de mensagem exclusivo KnowNetwork's")
-              .setTimestamp();
-            msg.reply(embed);
-        });
+        await db.query("SELECT guild_id,prefix FROM prefixs WHERE guild_id= ?", [Number(args[1])]);
+        if (result == "") return msg.reply('Servidor não encontrado.')
+        	.then((m) => { m.delete({timeout: 4000}).catch(() => null)} );
+        const prefix = JSON.parse(JSON.stringify(result[0].prefix));
+        let emoteFix = await client.emojis.cache.get("755967379363856455");
+        let embed = new Discord.MessageEmbed()
+        	.setColor("#000000")
+			.setTitle(`${emoteFix} **Prefixo ${client.guilds.cache.get(args[1]).name}!**`)
+            .setDescription(`\`\`\`o prefixo de ${client.guilds.cache.get(args[1]).name} é ${prefix}\`\`\``)
+            .setThumbnail(client.guilds.cache.get(args[1]).iconURL())
+            .setFooter("Sistema de mensagem exclusivo KnowNetwork's")
+            .setTimestamp();
+          msg.reply(embed);
     }
 };                                       
