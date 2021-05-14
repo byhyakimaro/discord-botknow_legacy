7af2fcfa7ecacc3fd9f921c219ff15aacae5de40
hyakimaro <45545324+byhyakimaro@users.noreply.github.com>
2021-05-14 14:59:50 -0300

    Add files via upload

--- scripts/db.js
+++ scripts/db.js
@@ -1,6 +1,20 @@
-const mysql = require("mysql");
+const mysql = require("mysql2/promise");
 const config = require("../config.json");
 
-module.exports = {
-  'mysql': mysql.createConnection({host: (config.host),user: (config.user),password: (config.password),database: (config.database)})
+async function connect(){
+    if(global.connection && global.connection.state !== 'disconnected')
+        return global.connection;
+    
+    const connection = await mysql.createConnection(`mysql://${config.user}:${config.password}@${config.host}:3306/${config.database}`);
+    console.log("Connected Database!");
+    global.connection = connection;
+    return connection;
 }
+
+async function query(sql, values){
+    const conn = await connect();
+    const [rows] = await conn.query(sql ,values);
+    return rows;
+}
+
+module.exports = {query}
